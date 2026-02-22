df6be4da51922d079040d2386458fea0d65cdc59
KasperJack <boubakeradem47@gmail.com>
2026-02-22 19:21:40 +0100

    added intial project files

---
+++ igdb.py
@@ -0,0 +1,61 @@
+import requests
+from dotenv import load_dotenv
+import os 
+import time
+
+load_dotenv()
+
+
+
+
+def search_games(query: str):
+
+    headers = {
+        "Client-ID": os.getenv("CLIENT-ID"),
+        "Authorization": "Bearer " + os.getenv("AUTHORIZATION")
+    }
+
+    res = requests.post("https://api.igdb.com/v4/games",headers=headers,data=f'search "{query}"; fields id; limit 50;')
+
+    ids = [str(g["id"]) for g in res.json()]
+    
+    if not ids:
+        return []
+    id_list = ",".join(ids)
+
+    res2 = requests.post("https://api.igdb.com/v4/games",headers=headers,
+                         data=f"""
+                            fields name, slug, first_release_date;
+                            where id = ({id_list})
+                            & platforms = (6)
+                            & version_parent = null
+                            & game_type = 0
+                            & first_release_date != null
+                            & first_release_date < {int(time.time())};
+                            limit 50;
+                         """)
+
+    for g in res2.json():
+        print(g["name"])
+    print('==================')
+
+
+    res3 = requests.post("https://api.igdb.com/v4/games",headers=headers,
+                         data=f"""
+                            search "{query}";
+                            fields name, slug, first_release_date;
+                            where platforms = (6)
+                            & version_parent = null
+                            & game_type = 0
+                            & first_release_date != null
+                            & first_release_date < {int(time.time())};
+                            limit 50;
+
+                         """)
+    for g in res3.json():
+        print(g["name"])
+
+
+
+
+search_games("witcher")
---
+++ main.py
@@ -0,0 +1,92 @@
+import requests
+from bs4 import BeautifulSoup
+import uuid
+
+#from urllib.parse import urljoin
+
+base_url = "https://steamunlocked.org/"
+query = "gun"
+
+#response = requests.get(f"{base_url}/?s={query}", headers={"User-Agent": "Mozilla/5.0"})
+#soup = BeautifulSoup(response.text, "html.parser")
+#print(soup)
+
+class Game:
+    def __init__(self,id: uuid.UUID,title: str,download_page: str):
+        self.id = id
+        self.title = title
+        self.download_page = download_page
+
+
+class SearchResult:
+    def __init__(self,parent_site: "site",query: str,games: dict[uuid.UUID,Game]):
+        self.parent_site = parent_site
+        self.query = query
+        self.games = games
+        
+        self.next = None
+        self.prev = None
+
+
+    def get_next(self):
+        pass
+    def get_prev(self):
+        pass
+
+
+
+
+
+
+class Steamunlocked:
+    def __init__(self):
+        self.base_url = "https://steamunlocked.org/"
+    
+    def search_game(self,query: str) -> SearchResult | None:
+        query = query.strip()
+        query = query.replace(" ","+")
+
+        response = requests.get(f"{base_url}/?s={query}", headers={"User-Agent": "Mozilla/5.0"})
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        first_h1 = soup.find("h1")
+        if first_h1.get_text() == "No results found":
+            return None
+
+
+        games: dict[uuid.UUID,Game] = {}
+
+        for h1 in soup.find_all("h1"):
+            #print(h1)
+            title = h1.get_text(strip=True)
+            parent_link = h1.find_parent("a")
+            href = parent_link["href"] if parent_link else "No link"
+
+            id = uuid.uuid4()
+            g = Game(id,title,href)
+            games[id] = g
+            #print(f"{title} -> {href}")
+
+
+        return SearchResult(self,query,games)
+            
+
+
+
+
+
+
+def run_test():
+    site = Steamunlocked()
+    r: SearchResult | None = site.search_game("far cry")
+    if r:
+        print(r.games)
+        for v in r.games.values():
+
+            print(v.title)
+
+        r.get_next()
+
+
+
+run_test()
