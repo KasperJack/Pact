df04c04e33e6436e547a7d5561fd3e93f162482d
KasperJack <boubakeradem47@gmail.com>
2026-03-19 15:16:33 +0100

    resolver  #1

--- core/resolver.py
+++ core/resolver.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from typing import Any
-
+from .exceptions import NotFoundError, AutoSelectError
 
 
 
@@ -9,13 +9,15 @@ class resolver:
         self.package_name = package_name
         self.package_path = package_path
         self.index_data = index_data
+        
+        # User input (can be None)
         self.target_source = source
         self.target_version = version
         self.target_method = method
 
-        self.avalable_sources = []
-        self.avalable_versions = []
-        self.avalable_methods = []
+        self.available_sources = []
+        self.available_versions = []
+        self.available_methods = []
 
         self.resolve()
 
@@ -23,11 +25,11 @@ class resolver:
 
     def resolve(self):
 
-        self.get_avalable_sources() ##throws an error no avalbale sources  
+        self.get_available_sources() ##throws an error no avalbale sources  
 
         if self.target_source:
-            if self.target_source not in self.avalable_sources:
-                raise SomeError(self) #" fix this"
+            if self.target_source not in self.available_sources:
+                raise NotFoundError("source", self.target_source, self.available_sources)
         else:
             self.auto_select_source()
 
@@ -38,14 +40,34 @@ class resolver:
 
 
 
-    def get_avalable_sources(self):
+    def get_available_sources(self):
 
         package_path = Path(self.package_path)
-        self.avalable_sources = [
+        self.available_sources = [
         p.name
         for p in package_path.iterdir()
         if p.is_dir() and p.name != "steam_builds"
-    ]
+        ]
+
+        if len(self.available_sources) == 0:
+            raise AutoSelectError("source",self.package_name)
+
+
+    def auto_select_source(self):
+        pref_sources = ["a","b","c"]
+        default = self.index_data.get("default_version")
+
+        if default:
+            source, _ = default.split("/", 1) #!? 
+            return source
+        
+        for s in pref_sources:
+            if s in sources:
+                return s
+
+   
+
+
 
 
 
