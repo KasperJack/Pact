b9dc93987b41c67d763a6db51a33f59474c03198
KasperJack <boubakeradem47@gmail.com>
2026-05-21 17:24:47 +0100

    wip

---
+++ corelib/build/state.go
@@ -0,0 +1,15 @@
+package build
+
+import (
+
+    //"fmt"
+    //"Pact/corelib/client"
+    "Pact/corelib/model"
+	"path/filepath"
+)
+
+
+func buildState (rootDir string) ([]model.PackageContent,error) {
+
+	return nil,nil
+}
