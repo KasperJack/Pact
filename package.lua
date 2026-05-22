e82766ffc464ea1f5ed372b9206a864cbbd0a8c6
KasperJack <boubakeradem47@gmail.com>
2026-05-22 18:37:40 +0100

    hello lua :)

--- package.lua
+++ package.lua
@@ -1,15 +1,10 @@
 package {
     package_identifier = "ffd",
-    -- name               = "Python",
-    -- versioning         = "semver",
+    name               = "Python",
+    versioning         = "semver",
 
     description = "Python programming language",
     homepage    = "https://python.org",
     license     = "PSF",
 }
 
-
-for i = 1, 3 do
-   
-    print(i) -- Outputs: 1, 2, 3
-end
